f227b15f26db2fba03898f0f2f0c252428a12669
Eugeniu Cazmal <eugeniucazmal@gmail.com>
2026-02-08 10:58:25 +0200

    feat: Moved smart P&L charts to main page after KPI cards

--- src/pages/PL.jsx
+++ src/pages/PL.jsx
@@ -583,6 +583,12 @@ const PLDashboard = () => {
                         />
                     </div>
 
+                    {/* SMART P&L CHARTS - MAIN PAGE */}
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                        <ProfitForecastChart monthlyData={visualizationData.monthlyProfits} />
+                        <LocationROIChart monthlyData={monthlyData} />
+                    </div>
+
                     {/* TAB NAVIGATION */}
                     <div className="flex gap-2 mb-6 bg-slate-100 dark:bg-slate-800/50 p-2 rounded-xl">
                         <button
@@ -689,12 +695,6 @@ const PLDashboard = () => {
                                     pl: m.plByLoc.reduce((sum, loc) => sum + loc.pl, 0)
                                 }))}
                             />
-
-                            {/* SMART P&L CHARTS */}
-                            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                                <ProfitForecastChart monthlyData={visualizationData.monthlyProfits} />
-                                <LocationROIChart monthlyData={monthlyData} />
-                            </div>
 
                             {/* SLOT OPTIMIZER */}
                             <SlotOptimizer locationData={optimizerData} />
