e5fe2e2bd06cb2624fc8a7e8a907240c0bae568b
Eugeniu Cazmal <eugeniucazmal@gmail.com>
2026-02-08 10:58:56 +0200

    fix: Moved smart P&L charts to bottom of main page

--- src/pages/PL.jsx
+++ src/pages/PL.jsx
@@ -583,12 +583,6 @@ const PLDashboard = () => {
                         />
                     </div>
 
-                    {/* SMART P&L CHARTS - MAIN PAGE */}
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                        <ProfitForecastChart monthlyData={visualizationData.monthlyProfits} />
-                        <LocationROIChart monthlyData={monthlyData} />
-                    </div>
-
                     {/* TAB NAVIGATION */}
                     <div className="flex gap-2 mb-6 bg-slate-100 dark:bg-slate-800/50 p-2 rounded-xl">
                         <button
@@ -702,6 +696,13 @@ const PLDashboard = () => {
                     )}
 
                 </div>
+
+                {/* SMART P&L CHARTS - BOTTOM OF PAGE */}
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                    <ProfitForecastChart monthlyData={visualizationData.monthlyProfits} />
+                    <LocationROIChart monthlyData={monthlyData} />
+                </div>
+
             </div>
         </Layout>
     )
